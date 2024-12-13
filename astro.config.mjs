f30d195998f8a959e299d128ef0767a490a50270
Steven Tam <60487779+steven-tam@users.noreply.github.com>
2024-12-13 12:46:49 -0600

    removed mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  site: "https://steventam.com",
+  integrations: [sitemap(), tailwind()],
 });
