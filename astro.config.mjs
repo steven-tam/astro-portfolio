6e8c25c694ba4fb0ce309b79fe04947d251ba61d
Steven Tam <60487779+steven-tam@users.noreply.github.com>
2024-12-19 17:15:40 -0600

    revert to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   site: "https://www.steventam.com",
   integrations: [sitemap(), tailwind()],
   server: { host: '0.0.0.0'},
